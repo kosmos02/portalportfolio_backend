41bab5a0e3c394f02232912f77fbe2b27c6e59c9
kosmos02 <alexandrgabe@gmail.com>
2021-02-13 16:10:02 -0700

    added projects in login method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,7 @@ class UsersController < ApplicationController
         if @user && @user.authenticate(params[:user][:password])
             @token = JWT.encode([user_id: @user.id], ENV['SECRET_KEY_BASE'])
         
-            render json: {user: @user, token: @token}, include: [:portfolios]
+            render json: {user: @user, token: @token}, include: [:portfolios => {include: :projects}]
         else
             render json: {error: "Invalid Credentials"}, status: :unauthorized
         end
